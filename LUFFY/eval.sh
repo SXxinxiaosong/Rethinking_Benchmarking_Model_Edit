2ab6d95d162f2b20b2868190c6fd3f5c04dc00de
xxs <105906157+SXxinxiaosong@users.noreply.github.com>
2025-05-16 18:25:58 +0800

    Update eval.sh

--- LUFFY/eval.sh
+++ LUFFY/eval.sh
@@ -5,18 +5,9 @@ OUTPUT_DIR=./results_new/
 mkdir -p $OUTPUT_DIR
 
 # If you want to evaluate other models, you can change the model path and name.
-#/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_ROME_2025-05-13_09-54-48_step10
-#MODEL_PATH=/fs-computility/ai-shen/songxin/models/DeepSeek-R1-Distill-Llama-8B
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_ROME_2025-05-13_09-54-48_step10
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_ROME_2025-05-13_10-24-33_step100
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_AlphaEdit_2025-05-13_09-57-06_step10
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_MEND_2025-05-13_09-57-21_step10
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_RECT_2025-05-13_10-04-28_step10
+#MODEL_PATH=DeepSeek-R1-Distill-Llama-8B
 
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_AlphaEdit_2025-05-13_10-25-33_step100
-MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_MEND_2025-05-13_09-57-22_step100
-#MODEL_PATH=/fs-computility/ai-shen/songxin/EasyEdit_for_reasoning_LLM/examples/saved_model/DeepSeek-R1-Distill-Llama-8B_RECT_2025-05-13_10-30-48_step100
-MODEL_NAME=DeepSeek-R1-Distill-Llama-8B_MEND_100
+MODEL_NAME=DeepSeek-R1-Distill-Llama-8B
 
 
 
